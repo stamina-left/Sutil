9cbfd8a4db23d83d76e6f95377efb0a66a0d2089
Albert Pangestu <stamina.left@gmail.com>
2020-09-22 21:48:28 +0700

    Initial Commit

---
+++ Sutil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sutil
+//
+//  Created by Albert Pangestu on 22/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sutil/SutilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SutilApp.swift
+//  Sutil
+//
+//  Created by Albert Pangestu on 22/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SutilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
